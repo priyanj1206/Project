82d5a19b9e5651a4713fec206feb515aea8cd086
Priya <Priya@LAPTOP-MI1A0149>
2022-05-09 12:27:59 +0530

    EvenOdd

---
+++ Project1/src/Even.java
@@ -0,0 +1,18 @@
+class Even 
+{
+   public static void main(String args[]) 
+		{
+			int num = 10;
+			if ((num & 1) == 0)
+			{
+				System.out.println(num + "is even");
+			}
+			else 
+			{ 
+				System.out.println(num + "is odd");	
+			}
+		}
+
+	}
+
+
